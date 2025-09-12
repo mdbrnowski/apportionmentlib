c42a80f7181bd06bdf9998d1d761c403a4408e89
Michał Dobranowski <mdbrnowski@gmail.com>
2025-09-13 00:03:21 +0200

    Create `is_population_monotone` and `Election`

--- Apportionmentlib/Basic.lean
+++ Apportionmentlib/Basic.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2025 Michał Dobranowski. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Michał Dobranowski
+-/
 import Mathlib.Algebra.BigOperators.Group.Finset.Basic
 import Mathlib.Data.Finset.Basic
 import Mathlib.Data.Rat.Init
@@ -15,19 +20,40 @@ instance : ToString Party where
 instance : Repr Party where
   reprPrec p _ := s!"{p.name} : Party"
 
+/-- An election with a finite set of parties, a function giving the number of votes for each party,
+and the total number of seats to be allocated. -/
+structure Election where
+  parties : Finset Party
+  votes : Party -> ℕ
+  total_seats : ℕ
+  deriving Inhabited
+
+def Election.total_voters (election : Election) : ℕ :=
+  ∑ p ∈ election.parties, election.votes p
+
 def Apportionment : Type := Party -> ℕ
-def Election : Type := Party -> ℕ
-def Rule : Type := Election -> ℕ -> Apportionment
+def Rule : Type := Election -> Apportionment
 
 /-- A rule `r` is a quota rule if the number of seats allocated to each party `p` is either the
 floor of its quota or the ceiling of its quota. -/
-def is_quota_rule (r : Rule) (parties : Finset Party) (election : Election) (num_seats : ℕ)
-    : Prop :=
-  let num_voters := ∑ p ∈ parties, election p
-  ∀ p ∈ parties,
-    let quota := (election p * num_seats : ℚ) / (num_voters : ℚ)
-    let num_party_seats := r election num_seats p
-    quota.floor ≤ num_party_seats ∧ num_party_seats ≤ quota.floor
+def is_quota_rule (rule : Rule) : Prop :=
+  ∀ election : Election,
+    let num_voters := election.total_voters
+    ∀ p ∈ election.parties,
+      let quota := (election.votes p * election.total_seats : ℚ) / (num_voters : ℚ)
+      rule election p = quota.floor ∨ rule election p = quota.ceil
+
+/-- A rule `r` is population monotone if when the number of votes for a party `p` increases, but the
+total number of votes remains the same, then the number of seats allocated to `p` does not decrease.
+-/
+def is_population_monotone (rule : Rule) : Prop :=
+  ∀ election₁ election₂ : Election,
+    election₁.parties = election₂.parties ∧
+    election₁.total_seats = election₂.total_seats ∧
+    election₁.total_voters = election₂.total_voters →
+    ∀ p ∈ election₁.parties,
+      election₁.votes p < election₂.votes p →
+      rule election₁ p ≤ rule election₂ p
 
 
 #eval Party.mk "A"
